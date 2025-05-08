15daf94187b79436dc3c867323416153e2d73249
Robert Habermeier <rphmeier@gmail.com>
2025-05-08 17:03:01 -0500

    debug + fix ref img encoding

--- uno/flux/model.py
+++ uno/flux/model.py
@@ -224,9 +224,10 @@ class Flux(nn.Module):
         ref_imgs: list[Tensor] | None = None,
         **kwargs,
     ) -> Tensor:
+        print(f"got ref imgs={ref_imgs}")
         bs, c, h, w = x.shape
         img, img_ids = prepare_img_encoding(x)
-        ref_img, ref_img_ids = prepare_ref_img_encoding(img, ref_imgs, x.device)
+        ref_img, ref_img_ids = prepare_ref_img_encoding(x, ref_imgs, x.device)
         txt, txt_ids, y = final_prompt_encoding(bs, context, y)
 
         x = comfy.ldm.common_dit.pad_to_patch_size(x, (2, 2))
